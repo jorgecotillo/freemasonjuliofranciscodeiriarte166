d6150c3a3348eee0b1b603e478fc5362d08b8c71
Jorge Cotillo <jorge.cotillo@gmail.com>
2017-02-26 22:17:09 -0500

    Adding temporally new dns'

--- presentation/ClientApp/app/components/shared/services/auth.service.ts
+++ presentation/ClientApp/app/components/shared/services/auth.service.ts
@@ -196,14 +196,14 @@ export class AuthService {
 }
 
 const settings: any = {
-  authority: 'http://localhost:5000',
+  authority: 'http://identity.provider.cotillo-corp.com/',
   client_id: 'js',
-  redirect_uri: 'http://localhost:4040/callback',
-  post_logout_redirect_uri: 'http://localhost:4040/home',
+  redirect_uri: 'http://juliofranciscodeiriarte166.org/callback',
+  post_logout_redirect_uri: 'http://juliofranciscodeiriarte166.org/home',
   response_type: 'id_token token',
   scope: 'openid profile email api1',
 
-  silent_redirect_uri: 'http://localhost:4040/home',
+  silent_redirect_uri: 'http://juliofranciscodeiriarte166.org/home',
   //automaticSilentRenew: true,
   //silentRequestTimeout:10000,
 
